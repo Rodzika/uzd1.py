6bfa97c12de370f9213d6a1833813404a5540a55
Rodzika <Kendijarodzika20@gmail.com>
2024-10-28 13:12:19 +0200

    Add files via upload

---
+++ uzd1.py
@@ -0,0 +1,21 @@
+x=(input("Enter x:"))
+y=(input("Enter y:"))
+
+result= 4*(2**(x*y)) / (x * y) 
+ 
+
+
+result= 5 * x ** 3 - x ** 2 + 7 * x - 6
+ 
+
+ 
+result= (x*y)**0.5
+ 
+
+ 
+result= 2*(x*y) / 5*y
+
+#2.uzd
+
+x==1
+y==2
